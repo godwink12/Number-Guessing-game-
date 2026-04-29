a6be17d96aa8023e24c0877efd87749befd1e015
Godwin <135100112+godwink12@users.noreply.github.com>
2026-04-29 20:56:43 +0300

    add while loop

--- Number Guessing game.py
+++ Number Guessing game.py
@@ -4,6 +4,13 @@ secret_number = rand.randint(1,100)
 succeed = False
 user_tries = 3
 
+while True:
+    # User Input
+    user_input = input('Enter the secret number: ')
 
+    # Check user Input 
+    if user_input == secret_number :
+        print ('Congratulation You guessed the Secret Number')
+        succeed = True
 
 
