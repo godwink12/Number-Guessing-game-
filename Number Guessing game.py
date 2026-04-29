98a9771cf01b8aca9b3831c25aaa4f8ef33e9d87
Godwin <135100112+godwink12@users.noreply.github.com>
2026-04-29 21:56:29 +0300

    add comments

--- Number Guessing game.py
+++ Number Guessing game.py
@@ -7,23 +7,22 @@ user_tries = 0
 while True:
     # User Input
     user_input = int(input('Enter the secret number: '))
-    if user_input < 1 or user_input > 100:
+    if user_input < 1 or user_input > 100: # Check User Stay on Number range
         print('Out of bonds! Stay between 1 and 100')
         continue
 
-    user_tries += 1
+    user_tries += 1 # Increment 1 each time the User Get it Wrong
     # Check user Input 
     if user_input == secret_number :
         print ('Congratulation You guessed the Secret Number!!!')
         break
     elif user_input > secret_number :
-        print('Hey almost there the Number is Lower!')
+        print('Hey almost there the Number is Lower!') # Print Hint
     elif user_input < secret_number :
-        print('Hey almost there the Number is Higher!')
+        print('Hey almost there the Number is Higher!') # Print Hint
     else :
         print ('Wrong Try again!!!')
-        user_tries += 1
-    if user_tries == 3 :
+    if user_tries == 3 : 
         print ('Nice try!!! You exceeded your Tries The Answer Was :',secret_number)
         break
     
