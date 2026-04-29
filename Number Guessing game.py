2cf2f85c6454f668363bc649d2d6519f2fb6911b
Godwin <135100112+godwink12@users.noreply.github.com>
2026-04-29 21:53:36 +0300

    add if /elif/else statement

--- Number Guessing game.py
+++ Number Guessing game.py
@@ -2,15 +2,30 @@ import random as rand
 
 secret_number = rand.randint(1,100)
 succeed = False
-user_tries = 3
+user_tries = 0
 
 while True:
     # User Input
-    user_input = input('Enter the secret number: ')
+    user_input = int(input('Enter the secret number: '))
+    if user_input < 1 or user_input > 100:
+        print('Out of bonds! Stay between 1 and 100')
+        continue
 
+    user_tries += 1
     # Check user Input 
     if user_input == secret_number :
-        print ('Congratulation You guessed the Secret Number')
-        succeed = True
+        print ('Congratulation You guessed the Secret Number!!!')
+        break
+    elif user_input > secret_number :
+        print('Hey almost there the Number is Lower!')
+    elif user_input < secret_number :
+        print('Hey almost there the Number is Higher!')
+    else :
+        print ('Wrong Try again!!!')
+        user_tries += 1
+    if user_tries == 3 :
+        print ('Nice try!!! You exceeded your Tries The Answer Was :',secret_number)
+        break
+    
 
 
